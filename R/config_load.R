0cd6e36a87127f72ba4fba5b713c56d3393c798e
hasruleng <hasrulwho@gmail.com>
2024-11-24 22:09:32 +0100

    rename load_vars into load_variables

--- R/config_load.R
+++ R/config_load.R
@@ -4,18 +4,18 @@ config_load <- function() {
   config_file <- here::here("tests/testthat/env.yml")
 
   if (file.exists(config_file)) { # non rcmdcheck use cases: package, source code, normal unittests
-    load_vars(config_file)
+    load_variables(config_file)
 
     # user_config_file is used by users to replace or supplement default configuration
     user_config_file <- here::here("config.yml")
     if (file.exists(user_config_file)) { # optionally load the user config_file
-      load_vars(user_config_file)
+      load_variables(user_config_file)
     }
   } else { # rcmdcheck case or env.yml is missing
     print("=========== rcmdcheck ?? =======")
     config_file <- here::here("env.yml") # rcmdcheck working directory is in testthat
     if (file.exists(config_file)) {
-      load_vars(config_file)
+      load_variables(config_file)
     } else {
       warning("The env config file does not exist. Please check the file path.")
       warning(config_file)
@@ -23,13 +23,13 @@ config_load <- function() {
   }
 }
 
-load_vars <- function(config_file_path) {
+load_variables <- function(config_file_path) {
   # Load the YAML file
+  print("config file message: ")
   config_file_message <- paste("=== loading into environment variables: ", config_file_path)
 
   config <- yaml::yaml.load_file(config_file_path)
   config_file_message <- paste(config_file_message, "\n", config)
-  print("config file message: ")
   print(config_file_message)
   # Set environment variables
   set_env_vars <- function(config_list, prefix = "") {
